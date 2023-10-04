a2b274e502bc3ad9b595fd4da2556b2661062b84
we4er <neznajko@yahoo.com>
2023-10-04 14:28:08 +0300

    📴

--- odin/ruby/caesar_cipher.rb
+++ odin/ruby/caesar_cipher.rb
@@ -1,6 +1,25 @@
 ########################################################
-def caesar_cipher text, shift
-  p text, shift
+$MOD = 26
+########################################################
+def decode char, shift
+  if char >= 'a' and char <= 'z'
+    base = 'a'.ord
+  elsif char >= 'A' and char <= 'Z'
+    base = 'A'.ord
+  else
+    return char
+  end
+  off = ( char.ord - base ) + shift
+  ( base + off % $MOD ).chr
+end
+#######################################################_
+def caesar_cipher msg, shift
+  cipher = ""
+  msg.each_char do |char|
+    cipher << decode( char, shift )
+  end
+  cipher
 end
 ########################################################
-caesar_cipher( "What a string!", 5 )
+puts caesar_cipher( "What a string!", 5 )
+########################################################
